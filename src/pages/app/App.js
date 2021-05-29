bc37166eeec60c4eebcc43f75dc776dc1d73e9b1
Oleg Alexandrov <teratronsirimus@gmail.com>
2021-05-29 16:38:54 +0300

    Add root.css

--- src/pages/app/App.js
+++ src/pages/app/App.js
@@ -6,14 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        <p>
+        <span>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </span>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
         >
           Learn React
         </a>
