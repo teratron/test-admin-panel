2cf3a6afe5e46a5a7d18502a94df1eb47a725a59
Oleg Alexandrov <teratronsirimus@gmail.com>
2021-05-30 18:58:40 +0300

    Add first component.

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './Header.css';
+
+function Header() {
+    return (
+        <header className="App-header">
+        </header>
+    );
+}
+
+export default Header;
