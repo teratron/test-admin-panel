3c12c9b389a8c3aeef3e77833c3591c34af59ee2
Oleg Alexandrov <teratronsirimus@gmail.com>
2021-05-31 12:11:58 +0300

    Add Main component.

---
+++ src/components/Main/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Main.css';
+
+function Main() {
+    return (
+        <main className="App-main">
+            <aside>sidebar</aside>
+        </main>
+    );
+}
+
+export default Main;
