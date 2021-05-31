c5064800353095f52efc1fd3a09e1096a01e6aab
Oleg Alexandrov <teratronsirimus@gmail.com>
2021-05-31 19:37:49 +0300

    Add fontawesome.

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faCoffee} from '@fortawesome/free-solid-svg-icons'
 import './Sidebar.css';
 
 function Sidebar() {
     return (
-        <aside className="App-main-sidebar">sidebar</aside>
+        <aside className="App-main-sidebar">
+            <FontAwesomeIcon icon={faCoffee}/>
+            sidebar
+        </aside>
     );
 }
 
