9dde7ec7851a92c2bf94872ec1b02ffef4dfcdbf
Oleg Alexandrov <teratronsirimus@gmail.com>
2021-06-04 19:56:47 +0300

    Add page: dashboard and support components.

--- src/components/Content/Support.js
+++ src/components/Content/Support.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
-import ChartLine from '../Chart/Line';
 import Button from '../Button';
 import Table from '../Table';
 import Card from '../Card';
 
-function Dashboard() {
+function Support() {
     return (
         <>
-            <Card content={ChartLine}/>
             <Card content={<Table bordered striped/>}/>
             <Button text="CONFIGURING REPORT" color="secondary" bordered rounded shadowed/>
         </>
     );
 }
 
-export default Dashboard;
+export default Support;
--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 
-import ChartLine from '../Chart/Line';
-import Button from '../Button';
-import Table from '../Table';
-import Card from '../Card';
+import Dashboard from './Dashboard';
 import './content.scss';
 
 function Content() {
     return (
         <section className="app-main-content">
-            <Card content={ChartLine}/>
-            <Card content={<Table bordered striped/>}/>
-            <Button text="CONFIGURING REPORT" color="secondary" bordered rounded shadowed/>
+            <Dashboard/>
         </section>
     );
 }
