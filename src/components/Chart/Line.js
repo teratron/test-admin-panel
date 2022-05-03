047557b11f3c22311077995ef5ae3d4ac475388f
Oleg Alexandrov <teratronsirimus@gmail.com>
2022-05-03 14:07:59 +0300

    Update ChartJS

--- src/components/Chart/Line.js
+++ src/components/Chart/Line.js
@@ -1,8 +1,62 @@
 import React from 'react';
 
+import {
+    CategoryScale,
+    Chart as ChartJS,
+    Legend,
+    LinearScale,
+    LineElement,
+    PointElement,
+    Title,
+    Tooltip,
+} from 'chart.js';
 import {Line} from 'react-chartjs-2';
 
-const data = {
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+export const options = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'top',
+        },
+        title: {
+            display: true,
+            text: 'Chart.js Line Chart'
+        },
+    },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+    labels,
+    datasets: [
+        {
+            label: 'Dataset 1',
+            data: [12, 19, 3, 5, 2, 3],
+            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        },
+        {
+            label: 'Dataset 2',
+            data: [2, 3, 19, 12, 3, 5],
+            borderColor: 'rgb(53, 162, 235)',
+            backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        },
+    ],
+};
+
+
+/*const data = {
     labels: ['1', '2', '3', '4', '5', '6'],
     datasets: [
         {
@@ -25,12 +79,10 @@ const options = {
             },
         ],
     },
-};
+};*/
 
 function ChartLine() {
-    return (
-        <Line data={data} options={options}/>
-    )
+    return <Line data={data} options={options} type='line'/>
 }
 
 export default ChartLine;
