c6117d00aec46e8083634d7308361d1faa79a884
Oleg Alexandrov <teratronsirimus@gmail.com>
2022-05-03 11:57:49 +0300

    Update to react v18.1.0

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 import reportWebVitals from './reportWebVitals';
 import App from './components/App';
 import './static/scss/index.scss';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
     <React.StrictMode>
         <App/>
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your Index, pass a function
